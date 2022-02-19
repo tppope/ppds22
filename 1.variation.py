9fb985537ad63c558b8e347f81860f7ce6f35640
tomaspopik <tpopik1999@gmail.com>
2022-02-19 13:40:52 +0100

    feat: add zero numbers incrementation using two threads

--- 1.variation.py
+++ 1.variation.py
@@ -1,15 +1,26 @@
+from collections import Counter
+
 from fei.ppds import Thread
 
 
 class Shared:
     def __init__(self, size):
-        pass
+        self.indicator = 0
+        self.zero_numbers = [0] * size
 
 
 def do_increment(shared):
-    pass
+    while shared.indicator < len(shared.zero_numbers):
+        shared.zero_numbers[shared.indicator] += 1
+        shared.indicator += 1
 
 
 shared = Shared(1_000_000)
 
 t1 = Thread(do_increment, shared)
+t2 = Thread(do_increment, shared)
+t1.join()
+t2.join()
+
+counter = Counter(shared.zero_numbers)
+print(counter.most_common())
