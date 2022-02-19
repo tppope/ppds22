42ff22f484999c2a92ea379f6da7371af91d1807
tomaspopik <tpopik1999@gmail.com>
2022-02-19 14:23:07 +0100

    docs: add docs of classes and functions

--- 1.variation.py
+++ 1.variation.py
@@ -4,12 +4,22 @@ from fei.ppds import Thread
 
 
 class Shared:
+    """Wrapper of the shared variables among threads"""
+
     def __init__(self, size):
+        """Initialize indicator and set size of array with zero numbers
+
+        :param size: size of zero_numbers array
+        """
         self.indicator = 0
         self.zero_numbers = [0] * size
 
 
 def do_increment(shared):
+    """Increment elements which the indicator points to
+
+    :param shared: object with shared variables
+    """
     while shared.indicator < len(shared.zero_numbers):
         shared.zero_numbers[shared.indicator] += 1
         shared.indicator += 1
