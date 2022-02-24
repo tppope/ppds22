4f03a225d2913abbef9077a4e3ed6987a44f7749
tomaspopik <tpopik1999@gmail.com>
2022-02-24 12:36:38 +0100

    docs: add barrier example description

--- adt_simple_barrier.py
+++ adt_simple_barrier.py
@@ -7,6 +7,11 @@ from adt import SemaphoreSimpleBarrier
 
 
 def barrier_example(barrier, thread_id):
+    """Execution demonstration using sleep and then demonstrate barrier usage to keep all threads waiting for last one
+
+    :param barrier: synchronization pattern using some abstract data type for synchronization
+    :param thread_id: identification number of thread that executes this function
+    """
     sleep(randint(1, 10) / 10)
     print("Vlakno %d pred barierou" % thread_id)
     barrier.wait()
