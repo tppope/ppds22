01da9e482a0319b62f7e70f9cd09f15f26cd8968
tomaspopik <tpopik1999@gmail.com>
2022-02-24 18:07:58 +0100

    docs(adt): add class and methods description

--- adt.py
+++ adt.py
@@ -2,13 +2,23 @@ from fei.ppds import Mutex, Semaphore
 
 
 class SemaphoreSimpleBarrier:
+    """Synchronization pattern to achieve synchronization where all threads wait for each other before executing
+    critical area. It is using abstract data types Mutex and Semaphore for synchronization."""
+
     def __init__(self, n):
+        """Initialize number of threads to wait, counter and abstract data type Mutex and Semaphore for synchronization
+
+        :param n: number of threads to wait on barrier
+        """
         self.n = n
         self.counter = 0
         self.mutex = Mutex()
         self.semaphore = Semaphore(0)
 
     def wait(self):
+        """Simple barrier implementation with atomic counter incrementation and condition checking
+
+        """
         self.mutex.lock()
         self.counter += 1
         if self.counter == self.n:
@@ -16,4 +26,3 @@ class SemaphoreSimpleBarrier:
             self.semaphore.signal(self.n)
         self.mutex.unlock()
         self.semaphore.wait()
-
