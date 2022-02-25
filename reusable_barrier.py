873ab078bed37e1451b3b7968985f8e9e8183a41
tomaspopik <tpopik1999@gmail.com>
2022-02-25 22:58:32 +0100

    docs(reusable barrier): add functions descriptions

--- reusable_barrier.py
+++ reusable_barrier.py
@@ -7,16 +7,30 @@ from adt import ReusableEventSimpleBarrier
 
 
 def rendezvous(thread_name):
+    """A simple function to demonstrate the execution of a program using the sleep function.
+
+    :param thread_name: thread identification
+    """
     sleep(randint(1, 10) / 10)
     print('rendezvous: %s' % thread_name)
 
 
 def ko(thread_name):
+    """A simple function to demonstrate the execution of a program using the sleep function.
+
+    :param thread_name: thread identification
+    """
     print('ko: %s' % thread_name)
     sleep(randint(1, 10) / 10)
 
 
 def barrier_example(thread_name, barrier):
+    """Example function to demonstrate the use of a reusable barrier in an infinite cycle. To end the program,
+    it is necessary to forcibly interrupt the process.
+
+    :param thread_name: thread identification
+    :param barrier: synchronization pattern using some abstract data type for synchronization
+    """
     while True:
         barrier.wait()
         rendezvous(thread_name)
@@ -29,7 +43,7 @@ if __name__ == "__main__":
 
     simple_barrier = ReusableEventSimpleBarrier(threads_count)
 
-    threads = [Thread(barrier_example, 'Thread %d' % i, simple_barrier) for i in
-               range(threads_count)]
+    # infinite cycle is used, force quit is required to end of the program
+    threads = [Thread(barrier_example, 'Thread %d' % i, simple_barrier) for i in range(threads_count)]
     for thread in threads:
         thread.join()
