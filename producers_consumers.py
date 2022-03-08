a6a351acc8ead17b9a4d3b0afcc0d033e312c7e2
tomaspopik <tpopik1999@gmail.com>
2022-03-08 23:38:42 +0100

    chore: initialize shared object with adt's for producers and consumers

--- producers_consumers.py
+++ producers_consumers.py
@@ -1,6 +1,11 @@
+from fei.ppds import Mutex, Semaphore
+
+
 class Shared:
     def __init__(self, capacity):
-        pass
+        self.mutex = Mutex()
+        self.free = Semaphore(capacity)
+        self.items = Semaphore(0)
 
 
 def produce(shared):
