efa480be5dfb3aaf27b71e32bcbfbabed7305daa
tomaspopik <tpopik1999@gmail.com>
2022-03-09 18:10:29 +0100

    docs: add code documentation

--- producers_consumers.py
+++ producers_consumers.py
@@ -34,14 +34,19 @@ def produce(shared):
     :param shared: object with shared abstract data types
     """
     while True:
+
+        # time demonstration of item production
         sleep(randint(1, 10) / 10)
+
         shared.storage.wait()
         if shared.end:
             break
         shared.mutex.lock()
+
+        # time demonstration of item storage
         sleep(randint(1, 10) / 100)
+
         shared.mutex.unlock()
-        print("P")
         shared.items.signal()
 
 
@@ -55,10 +60,14 @@ def consume(shared):
         if shared.end:
             break
         shared.mutex.lock()
+
+        # time demonstration of item consumption
         sleep(randint(1, 10) / 100)
+
         shared.mutex.unlock()
         shared.storage.signal()
-        print("F")
+
+        # time demonstration of item processing
         sleep(randint(1, 10) / 10)
 
 
@@ -68,11 +77,16 @@ if __name__ == "__main__":
     consumers = [Thread(consume, synch) for _ in range(2)]
     producers = [Thread(produce, synch) for _ in range(5)]
 
+    # work time of producers and consumers
     sleep(5)
     synch.end = True
+
     print('Main thread: waiting for completion')
+
+    # release of producers and consumers who were waiting for their computation when notified of stop doing computation
     synch.storage.signal(100)
     synch.items.signal(100)
+
     [thread.join() for thread in producers + consumers]
 
     print('Main thread: complete')
