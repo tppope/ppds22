8f96c45225735f3327923f6bbeef231e14785e83
tomaspopik <tpopik1999@gmail.com>
2022-04-09 19:41:01 +0200

    docs: add code documentation

--- first_letter_capitalized.py
+++ first_letter_capitalized.py
@@ -8,11 +8,15 @@
 
 
 def capitalize_first_letter():
-    """
+    """Function in which a file is opened, and we create generators to which we send lines to capitalize the first
+    letter of each word and write it to a new file.
 
     """
+    # generator for write in file
     file_writing = write_file(open("new_text.txt", "w"))
+    # generator for capitalize words from file
     capitalize = split_and_capitalize(file_writing)
+    # generator for file reading
     file_reading = read_file(capitalize)
     next(file_reading)
     try:
@@ -23,12 +27,14 @@ def capitalize_first_letter():
             yield
     except GeneratorExit:
         print("\n----- Fist letter capitalized co-program -----\n")
+        file_reading.close()
 
 
 def read_file(capitalize):
-    """
+    """Function (generator) that reading a file and sending lines to the first letter capitalization function. On
+    calling close() on this generator, prints the number of files read and call close() on the capitalize generator.
 
-    :param capitalize:
+    :param capitalize: function (generator) for capitalize first letter of words
     """
     next(capitalize)
     file_count = 0
@@ -44,9 +50,10 @@ def read_file(capitalize):
 
 
 def write_file(file):
-    """
+    """Function (generator) that writes capitalized words to an open file and, after calling close() on this
+    generator, prints the total number of written words and close opened file.
 
-    :param file:
+    :param file: open file for writing
     """
     chars_count = 0
     try:
@@ -60,9 +67,10 @@ def write_file(file):
 
 
 def split_and_capitalize(file_writing):
-    """
+    """A function that capitalizes the first letter of words and sends them to the generator for writing. Calling
+    close() on this generator prints the number of capitalized words and calls close() on the write generator.
 
-    :param file_writing:
+    :param file_writing: function (generator) for write words in file
     """
     next(file_writing)
     capitalized_words_count = 0
