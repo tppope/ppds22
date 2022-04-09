bf57658a86bd5a1501db91689f7456a8853b5a7f
tomaspopik <tpopik1999@gmail.com>
2022-04-09 18:39:15 +0200

    feat: add capitalization of every word in file and write in new file

--- coroutines_via_enhanced_generators.py
+++ coroutines_via_enhanced_generators.py
@@ -2,6 +2,7 @@ from time import sleep
 
 from fei.ppds import Thread
 
+from first_letter_capitalized import capitalize_first_letter
 from primes_in_fibonacci import fibonacci_sequence
 
 
@@ -18,9 +19,12 @@ def scheduler(shared):
     :param shared:
     """
     fibonacci = fibonacci_sequence()
+    first_letter_capitalized = capitalize_first_letter()
     while not shared.is_over:
         next(fibonacci)
+        next(first_letter_capitalized)
     fibonacci.close()
+    first_letter_capitalized.close()
 
 
 def main():
---
+++ first_letter_capitalized.py
@@ -0,0 +1,55 @@
+def capitalize_first_letter():
+    file_writing = write_file(open("new_text.txt", "w"))
+    capitalize = split_and_capitalize(file_writing)
+    file_reading = read_file(capitalize)
+    next(file_reading)
+    try:
+        while True:
+            file = open("text.txt", "r")
+            file_reading.send(file)
+            file.close()
+            yield
+    except GeneratorExit:
+        print("\n----- Fist letter capitalized co-program -----\n")
+
+
+def read_file(capitalize):
+    next(capitalize)
+    file_count = 0
+    try:
+        while True:
+            file = yield
+            for line in file:
+                capitalize.send(line)
+            file_count += 1
+    except GeneratorExit:
+        print("Number of read file: %d" % file_count)
+        capitalize.close()
+
+
+def write_file(file):
+    chars_count = 0
+    try:
+        while True:
+            word = yield
+            chars_count += len(word)
+            file.write(word)
+    except GeneratorExit:
+        print("Number of written characters: %d" % chars_count)
+        file.close()
+
+
+def split_and_capitalize(file_writing):
+    next(file_writing)
+    capitalized_words_count = 0
+    try:
+        while True:
+            line = yield
+            for word in line.split(" "):
+                if "\n" not in word:
+                    word += " "
+                file_writing.send(word.capitalize())
+                capitalized_words_count += 1
+    except GeneratorExit:
+        print("Number of capitalized words: %d" % capitalized_words_count)
+        file_writing.close()
